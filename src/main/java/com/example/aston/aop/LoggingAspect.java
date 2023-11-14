953bebbe9c64cced4a8b62dfb84e76276e0ebb99
RibbleStripe <ribblestripeps3@gmail.com>
2023-11-14 13:38:06 +0300

    fix LoggingAspect

--- src/main/java/com/example/aston/aop/LoggingAspect.java
+++ src/main/java/com/example/aston/aop/LoggingAspect.java
@@ -5,23 +5,27 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
 public class LoggingAspect {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
     @Before("execution(* com.example.aston.service.*.*(..))")
     public void beforeMethodExecution(JoinPoint joinPoint) {
-        System.out.println("Executing method: " + joinPoint.getSignature().getName());
+        logger.info("Executing method: {}", joinPoint.getSignature().getName());
     }
 
     @AfterReturning(pointcut = "execution(* com.example.aston.service.*.*(..))", returning = "result")
     public void afterMethodExecution(JoinPoint joinPoint, Object result) {
-        System.out.println("Method " + joinPoint.getSignature().getName() + " executed. Result: " + result);
+        logger.info("Method {} executed. Result: {}", joinPoint.getSignature().getName(), result);
     }
 
     @AfterThrowing(pointcut = "execution(* com.example.aston.service.*.*(..))", throwing = "exception")
     public void afterMethodThrowing(JoinPoint joinPoint, Throwable exception) {
-        System.out.println("Error in method " + joinPoint.getSignature().getName() + ": " + exception.getMessage());
+        logger.error("Error in method {}: {}", joinPoint.getSignature().getName(), exception.getMessage());
     }
 }
